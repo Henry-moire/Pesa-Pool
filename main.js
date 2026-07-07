b2551600666a4e6797ce04d83dd931bd4373e7dc
Nyakundi <henrymoire@gmail.com>
2026-07-07 13:49:47 +0300

    Added app icon and build installer

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 1000,
         height: 700,
+        icon: path.join(__dirname, 'assets', 'icon.ico'),
         webPreferences: {
             contextIsolation: true,
             nodeIntegration: false,
